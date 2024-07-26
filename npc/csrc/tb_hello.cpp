6d749629f1083135855c315eae3c35c7792036ad
cll <1165442637@qq.com>
2024-07-26 09:21:26 +0800

    20240726

---
+++ npc/csrc/tb_hello.cpp
@@ -0,0 +1,17 @@
+  #include "Vhello.h"
+  #include "verilated.h"
+  
+  int main(int argc, char** argv) {
+      VerilatedContext* contextp = new VerilatedContext;
+      contextp->commandArgs(argc, argv);
+      Vhello* top = new Vhello{contextp};
+      
+      while (!contextp->gotFinish()) { 
+      top->eval(); 
+      }
+      
+      delete top;
+      delete contextp;
+      return 0;
+  }
+
